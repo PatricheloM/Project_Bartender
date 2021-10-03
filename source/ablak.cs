0dd0fcd036553bad1f7dbee8fb7abe2f7d9e0e56
PatricheloM <71276453+PatricheloM@users.noreply.github.com>
2021-10-03 02:58:51 +0200

    "Hello World!"
    Imported .NET libraries, added a simple window to the project with dynamic resolution

---
+++ source/ablak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bartender_M9D47D
+{
+    public partial class ablak : Form
+    {
+        public int ScreenWidth()
+        {
+            int width = Screen.PrimaryScreen.Bounds.Width;
+            return width;
+        }
+        public int ScreenHeight()
+        {
+            int height = Screen.PrimaryScreen.Bounds.Height;
+            return height;
+        }
+
+        public ablak()
+        {
+            InitializeComponent();
+            this.ClientSize = new System.Drawing.Size(ScreenWidth(), ScreenHeight());
+        }
+    }
+}
